6571fc726fb1e97739d0c6c3acabc4fdda250382
Rajiv Srivatsan <83394239+RajivSrivatsan@users.noreply.github.com>
2023-01-04 11:01:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+todos = []
 
+while True:
+    user_action = input("Enter add, show, edit or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'edit':
+            number = int(input("Number of to-do item to edit: "))
+            number -= 1
+            new_todo = input("Enter new to-do: ")
+            todos[number] = new_todo
+        case 'exit':
+            break
+        case _:
+            print("hey you entered an unknown command")
+
+print("Bye!")
